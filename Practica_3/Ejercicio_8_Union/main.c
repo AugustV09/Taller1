d2b0e6786c7541be0638d9c299a94bf2ac17439f
Octavio Isabella Valenzi <valenzi.octavio@gmail.com>
2024-04-12 10:38:08 -0300

    Terminando la practica 3

--- Practica_3/Ejercicio_8_Union/main.c
+++ Practica_3/Ejercicio_8_Union/main.c
@@ -5,21 +5,13 @@ typedef struct{
     char apellido[30];
     char nombre[30];
     int legajo;
-    int dni;
-}estArg;
+    int tipo;
+    union ide{
+        int dni;
+        char pasaporte[20];
+    } identidad;
+}estudiante;
 
-
-typedef struct{
-    char apellido[30];
-    char nombre[30];
-    int legajo;
-    char pasaporte[20];
-}estExt;
-
-typedef union{
-    estArg est1;
-    estExt est2;
-} estudiante;
 
 void leerEstudiante (estudiante *);
 void imprimir (estudiante);
@@ -28,12 +20,33 @@ int main()
 {
     estudiante e1,e2;
     leerEstudiante(&e1);
-    imprimir(e1)
+    imprimir(e1);
     leerEstudiante(&e2);
     imprimir(e2);
     return 0;
 }
 
 void leerEstudiante (estudiante *e){
-    printf("Ingrese el ")
+    printf("Ingrese el Nombre del estudiante: ");
+    scanf(" %s", e->nombre);
+    printf("Ingrese el Apellido del estudiante: ");
+    scanf(" %s", e->apellido);
+    printf("Ingrese el legajo del estudiante: ");
+    scanf(" %d", &e->legajo);
+    printf("Ingrese 0 si el estudiante es extranjero o cualquier otro numero en caso contrario: ");
+    scanf(" %d", &e->tipo);
+    if (e->tipo){
+        printf("Ingrese el DNI del estudiante: ");
+        scanf(" %d", &e->identidad.dni);
+    }
+    else{
+        printf("Ingrese el PASAPORTE del estudiante: ");
+        scanf(" %s", e->identidad.pasaporte);
+    }
+
+}
+
+void imprimir (estudiante e){
+    if (e.tipo) printf("El estudiante %s %s, de legajo: %d y de DNI: %d\n", e.apellido, e.nombre, e.legajo, e.identidad.dni);
+    else printf("El estudiante %s %s, de legajo: %d y de DNI: %s\n", e.apellido, e.nombre, e.legajo, e.identidad.pasaporte);
 }
