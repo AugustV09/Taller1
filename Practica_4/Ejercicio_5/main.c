4d36478d6073446eb7fe7bd12fbaa3d21395bba1
Octavio <valenzi.octavio@gmail.com>
2024-05-08 10:13:02 -0300

    Haciendo el punto 6 de la practica 4

--- Practica_4/Ejercicio_5/main.c
+++ Practica_4/Ejercicio_5/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ORACIONES
+#define ORACIONES 10
+
+void minMayus (char *);
 
 int main()
 {
@@ -9,8 +11,22 @@ int main()
     char *p = (char *) calloc(cant, sizeof(char));
 
     for (i = 0; i<ORACIONES; i++){
-
+        printf("Ingrese una oracion: \n");
+        fgets(p,cant,stdin);
+        minMayus(p);
     }
 
+    free(p);
+
     return 0;
+}
+
+void minMayus (char *p){
+    int mayus =0, minus =0,i;
+    for (i=0; p[i];i++){
+        if ((p[i]>= 'a') && (p[i]<='z')) minus++;
+        if ((p[i]>= 'A') && (p[i]<='Z')) mayus++;
+    }
+
+    printf("La oracion tiene %d minusculas y %d mayusculas\n", minus, mayus);
 }
