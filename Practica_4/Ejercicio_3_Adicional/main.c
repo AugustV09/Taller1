9999658c2fe59477f4806d9cdfbfe48c13d28c96
Octavio Isabella Valenzi <valenzi.octavio@gmail.com>
2024-05-17 11:56:38 -0300

    Terminando el ejercicio 3 de la practica adicional 4 pero me falta la funcion recursiva

--- Practica_4/Ejercicio_3_Adicional/main.c
+++ Practica_4/Ejercicio_3_Adicional/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define CANTB 10
+#define MAXIMO_MUESTRA 8
 
-typedef enum {MARCA='?', ES_BOMBA=0xeb, NO_ES_BOMBA=0} caracteres;
+typedef enum {MARCA='?', ES_BOMBA=0xeb, NO_ES_BOMBA=0, OCULTA = 0xB1} caracteres;
 
 typedef struct{
+    caracteres visibilidad;
     caracteres marca;
     caracteres esBomba;
     unsigned bomVecinas;
@@ -14,28 +16,36 @@ typedef struct {
     unsigned filas;
     unsigned columnas;
     unsigned casillas_si;
+    unsigned marcas_colocadas;
     casilla ** casillas;
 } tablero;
 
 void crearTablero(tablero*, unsigned, unsigned);
 void inicializarTablero (tablero);
 void jugada (tablero*,int*);
+void imprimirTablero (tablero);
+void mostrarCasillas (tablero*,int,int);
 
 int main()
 {
     srand(3);
     tablero T;
-    unsigned filas,columnas, perdedor=0;
+    unsigned filas,columnas;
+    int perdedor=0;
+    printf("Ingrese cantidad de filas: ");
     scanf("%u",&filas);
+    printf("Ingrese cantidad de columnas: ");
     scanf("%u",&columnas);
+    printf("\n");
     crearTablero(&T,filas,columnas);
 
     while (T.casillas_si>0 && !perdedor){
-        jugada(&tablero,&perdedor);
+        jugada(&T,&perdedor);
+        imprimirTablero(T);
     }
 
-    if (perdedor) printf("PERDISTE");
-    else printf("GANASTE");
+    if (perdedor) printf("PERDISTE\n");
+    else printf("GANASTE\n");
 
     return 0;
 }
@@ -44,6 +54,7 @@ void crearTablero(tablero *t, unsigned filas, unsigned columnas){
     t->filas = filas;
     t->columnas = columnas;
     t->casillas_si= filas*columnas - CANTB;
+    t->marcas_colocadas =0;
     t->casillas = (casilla **) malloc (filas*sizeof(casilla*));
 
     int i;
@@ -62,7 +73,7 @@ void inicializarTablero (tablero t){
     while (i<CANTB){
         f = rand()%8;
         c= rand()%8;
-        if !(t.casillas[f][c].esBomba){
+        if (!(t.casillas[f][c].esBomba)){
 
             t.casillas[f][c].esBomba = ES_BOMBA;
 
@@ -80,7 +91,7 @@ void inicializarTablero (tablero t){
             }
 
             if (f!=(t.filas-1) || c!=(t.columnas-1)){
-                if (c!=(t.columnas-1))}
+                if (c!=(t.columnas-1)){
                     t.casillas[f][c+1].bomVecinas+=1;
                     if (f!=(t.filas-1)) t.casillas[f+1][c+1].bomVecinas+=1;
                 }
@@ -89,7 +100,13 @@ void inicializarTablero (tablero t){
 
 
             }
-            i++
+            i++;
+        }
+    }
+
+    for (i=0; i<t.filas;i++){
+        for (j=0; j<t.columnas; j++){
+            t.casillas[i][j].visibilidad = OCULTA;
         }
     }
 
@@ -99,7 +116,7 @@ void inicializarTablero (tablero t){
 void jugada (tablero *t,int *perdedor){
     unsigned fila;
     unsigned char columna;
-    caracteres car =0;
+    char car =0;
 
     do{
         printf("Ingrese una fila entre 0 y 7: ");
@@ -109,11 +126,48 @@ void jugada (tablero *t,int *perdedor){
 
     do{
         printf("Ingrese una columna entre A y H: ");
-        scanf("%c",&columna);
+        scanf(" %c",&columna);
     } while (!(columna>= 'A' && columna<= 'H'));
 
-    printf("Ingrese '?' para marcar/desmarcar la casilla: ");
-    scanf("%d", &car);
+    columna -= 'A';
+    printf("Ingrese '?' para marcar/desmarcar la casilla y cualquier otro caracter en caso contrario: ");
+    scanf(" %c", &car);
+    printf("\n");
+
+    if (car == MARCA){
+        t->casillas[fila][columna].marca = car;
+        t->marcas_colocadas++;
+    }
+
+    else{
+        t->casillas[fila][columna].visibilidad =0;
+        if (t->casillas[fila][columna].esBomba == ES_BOMBA) *perdedor = 1;
+        else{
+            t->casillas_si--;
+            //if (t->casillas[fila][columna].bomVecinas == 0) mostrarCasillas(t,fila,columna);
+        }
+
+    }
+}
+
+void imprimirTablero (tablero t){
+    int i,j;
+
+    for (i=0; i<t.filas; i++){
+        for (j=0; j<t.columnas; j++){
+            if (t.casillas[i][j].marca == MARCA) printf("%c  ", MARCA);
+            else if (t.casillas[i][j].visibilidad == OCULTA) printf("%c  ", OCULTA);
+            else if (t.casillas[i][j].esBomba == ES_BOMBA) printf("%c  ", ES_BOMBA);
+            else printf("%u  ", t.casillas[i][j].bomVecinas);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+void mostrarCasillas (tablero *t, int fila, int columna){
+
 }
 
 
